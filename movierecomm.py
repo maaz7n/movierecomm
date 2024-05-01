386f2da5169d37e2343f95b2a18105533c1d6054
maaz7n <163116228+maaz7n@users.noreply.github.com>
2024-05-02 03:22:52 +0530

    Update movierecomm.py

--- movierecomm.py
+++ movierecomm.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import base64
 
 # Load the movie dataset
 @st.cache
@@ -34,17 +35,27 @@ def get_recommendations(movie_title, threshold=0.2):
     return recommendations
 
 # Streamlit UI
-st.markdown(
-    """
+    @st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
     <style>
-    .reportview-container {
-        background: url("https://raw.githubusercontent.com/maaz7n/movierecomm/main/background.jpg") no-repeat center center fixed;
-        background-size: cover;
+    body {
+    background-image: url("https://raw.githubusercontent.com/maaz7n/movierecomm/main/background.jpg,%s");
+    background-size: cover;
     }
     </style>
-    """,
-    unsafe_allow_html=True
-)
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 st.title('Movie Recommendation System')
 
