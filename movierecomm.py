6c310de3813ee7926ea913da36839084b89ba3a8
maaz7n <163116228+maaz7n@users.noreply.github.com>
2024-05-02 18:48:56 +0530

    Update movierecomm.py

--- movierecomm.py
+++ movierecomm.py
@@ -1,52 +1,38 @@
 import streamlit as st
 import pandas as pd
 
-# Set page background to a URL
-st.markdown(
-    """
-    <style>
-    body {
-        background-image: url("YOUR_BACKGROUND_IMAGE_URL");
-        background-size: cover;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Load movies and ratings data
-@st.cache
-def load_data():
-    movies = pd.read_csv("movies.csv")
-    ratings = pd.read_csv("ratings.csv")
-    return movies, ratings
+movies = pd.read_csv("movies.csv")
+ratings = pd.read_csv("ratings.csv")
 
 # Function to perform collaborative filtering
-def collaborative_filtering(movies, ratings, n=10):
-    # Group ratings by movie and count number of ratings for each movie
-    movie_ratings_count = ratings.groupby('movieId').size().reset_index(name='rating_count')
-    # Sort movies by rating count in descending order
-    popular_movies = movie_ratings_count.sort_values(by='rating_count', ascending=False)
-    # Get top N recommended movie IDs
-    top_movie_ids = popular_movies.head(n)['movieId']
-    # Get movie titles corresponding to top movie IDs
-    top_movies = movies[movies['movieId'].isin(top_movie_ids)]['title']
-    return top_movies
+def collaborative_filtering(user_id, n=10):
+    # Filter ratings for the given user
+    user_ratings = ratings[ratings['userId'] == user_id]
+    
+    # Merge user ratings with movie data
+    user_movie_ratings = pd.merge(user_ratings, movies, on='movieId')
+    
+    # Sort movies by rating in descending order
+    user_movie_ratings = user_movie_ratings.sort_values(by='rating', ascending=False)
+    
+    # Get top N recommended movies
+    top_movies = user_movie_ratings.head(n)
+    
+    return top_movies['title']
 
 # Streamlit UI
 st.title('Movie Recommendation System')
 
-# Load movies and ratings data
-movies, ratings = load_data()
+# User input for user ID
+user_id = st.number_input('Enter User ID', min_value=1, max_value=1000)
 
-# Search bar for user to search for movie names
-search_query = st.text_input('Search for a movie')
-
-# Filter movies based on search query
-filtered_movies = movies[movies['title'].str.contains(search_query, case=False)]
-filtered_movies = filtered_movies[filtered_movies['movieId'].isin(ratings['movieId'])]
-
-# Display filtered movies
-st.write('## Filtered Movies')
-for movie in filtered_movies['title']:
-    st.write('- ' + movie)
+# Button to trigger recommendation
+if st.button('Get Recommendations'):
+    # Perform collaborative filtering to get movie recommendations
+    recommended_movies = collaborative_filtering(user_id)
+    
+    # Display recommended movies
+    st.write('## Recommended Movies')
+    for movie in recommended_movies:
+        st.write('- ' + movie)
