cd77238e3a164acd791ef9c74efd32747dbde9f0
maaz7n <163116228+maaz7n@users.noreply.github.com>
2024-05-02 18:53:35 +0530

    Update movierecomm.py

--- movierecomm.py
+++ movierecomm.py
@@ -21,28 +21,17 @@ def load_data():
     ratings = pd.read_csv("ratings.csv")
     return movies, ratings
 
-# Function to filter movies based on search query
-def filter_movies(movies, search_query):
-    return movies[movies['title'].str.contains(search_query, case=False)]
-
 # Streamlit UI
 st.title('Movie Recommendation System')
 
 # Load data
 movies, ratings = load_data()
 
-# Search bar for user to search for movie names
-search_query = st.text_input('Search for a movie')
-
-# Filter movies based on search query
-filtered_movies = filter_movies(movies, search_query)
-
 # Scroll bar for user to choose movies
-selected_movies = st.multiselect('Choose movies', filtered_movies['title'])
+selected_movies = st.multiselect('Choose movies', movies['title'])
 
 # Display recommended movies
 if st.button('Get Recommendations'):
     st.write('## Recommended Movies')
     for movie_title in selected_movies:
-        movie_id = movies.loc[movies['title'] == movie_title, 'movieId'].iloc[0]
         st.write('- ' + movie_title)
