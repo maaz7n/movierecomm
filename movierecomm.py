6b6745c00888cc5daed3d5b8b14630d6ed4fa8c5
maaz7n <163116228+maaz7n@users.noreply.github.com>
2024-05-06 17:03:37 +0530

    Update movierecomm.py

--- movierecomm.py
+++ movierecomm.py
@@ -8,8 +8,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Function to load the movie dataset
 @st.cache
 def load_data():
+  return
    pd.read_csv("movies.csv")
-    return
+
 
 # Function to compute similarity matrix based on genres
 def compute_similarity_matrix(data):
